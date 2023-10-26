5012660ec4977025d3e01be8925f8fa3819d5a58
Keshav Thakur <keskumar2001@gmail.com>
2023-10-26 18:39:31 +0530

    Modified deleteAll btn feature

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -4,7 +4,11 @@ import { TodoContext } from '../context/TodoConetxt'
 const InputTodo = ({ prop }) => {
     const [todoText, setTodoText] = useState("")
     // const {todos,setTodos}= createContext(TodoContext);
+    const todos=prop.todos;
     const addTodo = () => {
+        if(todoText.length===0){
+            return;
+        }
         const todo = {
             id: Date.now(),
             text: todoText,
@@ -15,13 +19,24 @@ const InputTodo = ({ prop }) => {
     }
 
     const deleteAllTodos = () => {
-        prop.setTodos([]);
+        if(confirm("All Items get deleted")){
+            prop.setTodos([]);
+        }
+    }
+    //fn to calculate tasks todo
+    function tasksTodo(){
+        let unCompleted=0;
+        todos.map((todo)=>{
+            if(!todo.completed) unCompleted++;
+        })
+        return unCompleted;
     }
     return (
         <>
             <p >Add a TODO...</p>
             <div>
                 <input
+                    required={true}
                     value={todoText}
                     type="text" placeholder="What to do ?"
                     className="input-text"
@@ -35,11 +50,14 @@ const InputTodo = ({ prop }) => {
                 >ADD TODO</button>
             </div>
             <div id='deleteAllBtnDiv'>
+                <span><b>Tasks ToDo: {tasksTodo()}</b> </span>
+                {prop.todos.length >0 && 
                 <button
                     type="button"
                     className="btn btn-danger add-btn  dis" id="delete-all-btn"
                     onClick={deleteAllTodos}
                 >Delete All</button >
+                }
             </div>
         </>
     )
